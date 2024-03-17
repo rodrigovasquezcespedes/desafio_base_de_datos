02851933b17f4c4c60b1ce05a0d20971bc37eb51
rodrigo vasquez cespedes <rvasquez26@gmail.com>
2024-03-17 13:36:03 -0300

    modifica codigo de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,19 @@ import Alert from './components/Alert'
 const App = () => {
   const [colaboradores, setColaboradores] = useState(BaseColaboradores)
   const [alert, setAlert] = useState({ msg: '', color: '' })
+
   const eliminarColaborador = (id) => {
-    /* filter crea un nuevo arreglo de los colaboradores que son distintos al id seleccionado */
     const colaboradoresFiltrados = colaboradores.filter(colaborador => colaborador.id !== id)
     setColaboradores(colaboradoresFiltrados)
   }
+
   const buscarColaboradores = (terminoBusqueda) => {
-    /* filter crea un nuevo arreglo de los colaboradores que contengan el caracter marcados */
-    const colaboradoresFiltrados = BaseColaboradores.filter(colaborador =>
-      /* Object.values(colaborador) crea un array que devuelve los datos del objeto */
-      /* some es un método de array que verifica si al menos un elemento cumple con la condición */
-      Object.values(colaborador).some(value =>
-        typeof value === 'string' && value.toLowerCase().includes(terminoBusqueda.toLowerCase())
+    const resultadoBusqueda = colaboradores.filter((colaborador) =>
+      Object.values(colaborador).some((valor) =>
+        valor.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
       )
     )
-    setColaboradores(colaboradoresFiltrados)
+    setColaboradores(resultadoBusqueda)
   }
 
   return (
@@ -33,7 +31,8 @@ const App = () => {
         <div className='row'>
           <div className='col-12'>
             <div className='text-white p-3'>
-              <Buscador buscarColaboradores={buscarColaboradores} />
+              <h1>Listado de colaboradores</h1>
+              <Buscador onSearch={buscarColaboradores} />
             </div>
           </div>
           <div className='col-md-8 col-12'>
@@ -52,7 +51,9 @@ const App = () => {
                   <div className='col-12'>
                     <h3 className='text-dark text-center'>Agregar Colaborador</h3>
                     <Formulario setAlert={setAlert} colaboradores={colaboradores} setColaboradores={setColaboradores} />
-                    {alert.msg !== '' && <Alert msg={alert.msg} color={alert.color} />}
+                    <div className='p2'>
+                      {alert.msg !== '' && <Alert msg={alert.msg} color={alert.color} />}
+                    </div>
                   </div>
                 </div>
               </div>
--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,18 +1,9 @@
-import { useState } from 'react'
-
-const Buscador = ({ buscarColaboradores }) => {
-  const [terminoBusqueda, setTerminoBusqueda] = useState('')
-
-  /*
-  (event.target.value) extrae el valor del campo de entrada y lo usa para hacer la busqueda
-  buscarColaboradores(event.target.value)llamar a la función buscarColaboradores pasada como prop,pasando el valor del input */
-  const handleChange = (event) => {
-    setTerminoBusqueda(event.target.value)
-    buscarColaboradores(event.target.value)
-  }
-
+const Buscador = ({ onSearch }) => {
   return (
-    <input type='text' placeholder='Buscar colaborador' value={terminoBusqueda} onChange={handleChange} />
+
+    <input type='text' placeholder='Buscar Colaborador' onChange={(e) => onSearch(e.target.value)} />
+
   )
 }
+
 export default Buscador
